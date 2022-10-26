cb5d793062454938abd232b93af33e42c472243e
Fevzi Barış Demirel <83472010+DemirelBaris@users.noreply.github.com>
2022-10-26 15:48:57 +0000

    a

--- replit.nix
+++ replit.nix
@@ -1,6 +1,8 @@
 { pkgs }: {
-    deps = [
-        pkgs.nodejs-16_x
-        pkgs.cowsay
-    ];
+	deps = [
+		pkgs.nodejs-16_x
+        pkgs.nodePackages.typescript-language-server
+        pkgs.yarn
+        pkgs.replitPackages.jest
+	];
 }
