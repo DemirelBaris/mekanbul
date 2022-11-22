eafd4609d0c9f5bd90461bf5407ff01f678e13b1
DemirelBaris <bars.ant07@gmail.com>
2022-11-22 18:47:11 +0300

    odev5

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,5 +1,6 @@
 var mongoose=require('mongoose');
-var dbURI="mongodb+srv://mekan32:mekan123@mekanbul.62dzhgt.mongodb.net/?retryWrites=true&w=majority";
+//var dbURI = "mongodb://localhost/mekanbul";
+var dbURI="mongodb+srv://baris:mekan123@mekanbul.w2upcl1.mongodb.net/mekanbul?retryWrites=true&w=majority";
 mongoose.connect(dbURI);
 function kapat(msg,callback){
     mongoose.connection.close(function(){
